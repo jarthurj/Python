a19fb1eeeb731fb291f3d2a0f0c2b9ba77bcdbf9
Johnny <jaoldaker1@gmail.com>
2020-10-26 19:40:56 -0400

    hello world file for python

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+name = "Noelle"
+print("hello ", name)
+print("hello " + name)
+
+name = 42
+print("hello ", name)
+print("hello " + str(name))
+
+fav1 ="burgers"
+fav2 = "sushi"
+print("My favorite foods are {} and {}.".format(fav1, fav2))
+print(f"My favorite food are {fav1} and {fav2}.")
