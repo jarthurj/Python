467decb672d933c8471996171a45d99fd11631a3
Johnny <jaoldaker1@gmail.com>
2020-10-26 20:31:37 -0400

    loops

---
+++ loops1.py
@@ -0,0 +1,27 @@
+# BASIC
+# for x in range(1, 151, 1):
+# 	print(x)
+# Multiples of Five
+# for x in range(5,1001, 5):
+# 	print(x)
+# Count the Dojo Way
+# for x in range(1, 100, 1):
+# 	if x % 10 == 0:
+# 		print("Coding Dojo")
+# 	elif x % 5 == 0:
+# 		print("Coding")
+# Huge
+# sum = 0
+# for x in range(0, 500000, 1):
+# 	sum += x
+# print(sum)
+#count down by fours
+# for x in range(2018, 0, -4):
+# 	print(x)
+#Flex counter
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum, highNum + 1, 1):
+	if (x % mult == 0):
+		print(x)
