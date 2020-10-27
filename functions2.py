aeaf7ff9fc8baa52e4d6849d90be89c49b6355fe
Johnny <jaoldaker1@gmail.com>
2020-10-26 21:02:42 -0400

    functions 2

---
+++ functions2.py
@@ -0,0 +1,38 @@
+#countdown
+# def countdown(num):
+# 	lister = list()
+# 	for x in range(num, -1, -1):
+# 		lister.append(x)
+# 	return lister
+
+# print(countdown(7))
+
+#print and return
+# def print_and_return(pval, rval):
+# 	print(pval)
+# 	return rval
+
+#first plus length
+
+# def first_plus_length(lister):
+# 	return lister[0] + len(lister)
+
+# print(first_plus_length([1,2,3,4,5]))
+
+#value greater than second
+
+# def values_greater_than_second(lister):
+# 	ret_list = list()
+# 	for x in lister:
+# 		if x > lister[1]:
+# 			ret_list.append()
+# 	return ret_list
+
+#the length that value
+# def length_value(size, value):
+# 	ret_list = []
+# 	for x in range(0, size, 1):
+# 		ret_list.append(value)
+# 	return ret_list
+
+# print(length_value(4,7))
