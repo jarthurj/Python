fdcf759da009eb6dec2d9225800bdec1ab393207
Johnny <jaoldaker1@gmail.com>
2020-12-05 11:24:48 -0500

    users form assignment

--- _python/django/users_shell/users/views.py
+++ _python/django/users_shell/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
+from .models import Users
+def index(request):
 
-# Create your views here.
+	context  = {
+		'users_all' : Users.objects.all()
+	}
+	return render(request, 'index.html', context)
+
+def add_user(request):
+	new_user_first_name= request.POST['first_name']
+	new_user_last_name = request.POST['last_name']
+	new_user_email_address = request.POST['email_address']
+	new_user_age = request.POST['age']
+	Users.objects.create(first_name = new_user_first_name, last_name = new_user_last_name,
+						email_address = new_user_email_address, age = new_user_age)
+	return redirect('/')
